281c7c04a4c53ee557caf45ae5f4d5fd2eb60fd1
Alec Savoy <savoy.alec@yahoo.com>
2017-02-10 16:40:35 -0800

    Functional UI
    Functional UI for the Leafit app for IOS in Swift.

--- HelloCamera/ViewController.swift
+++ HelloCamera/ViewController.swift
@@ -1,25 +1,119 @@
 //
 //  ViewController.swift
-//  HelloCamera
+//  leafit2
 //
-//  Created by Alec Savoy on 2/5/17.
+//  Created by Alec Savoy on 2/7/17.
 //  Copyright © 2017 Alec Savoy. All rights reserved.
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject : String = ""
+    
+    @IBOutlet weak var takePhoto: UIButton!
+    
+    @IBOutlet weak var cameraRoll: UIButton!
+    
+    @IBOutlet weak var cameraStream: UIImageView!
+    var newMedia: Bool?
+    
+    @IBAction func useCamera(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.camera
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            newMedia = true
+        }
+    }
+    
+    @IBAction func useCameraRoll(_ sender: AnyObject){
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            newMedia = false
+        }
+    
+            
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        if mediaType.isEqual(to: kUTTypeImage as String) {
+            let image = info[UIImagePickerControllerOriginalImage]
+                as! UIImage
+            cameraStream.contentMode = UIViewContentMode.scaleAspectFill
+            cameraStream.image = image
+            
+            
+            if(newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self,
+                                               #selector(ViewController.image(image:didFinishSavingWithError:contextInfo:)), nil)
+            } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                print("video")
+            }
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer){
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated:true, completion: nil)
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ///self.dataLabel!.text = dataObject
+        ///self.dataLabel.text = dataObject
+        let back = UIImage(named: "background")
+        cameraStream = UIImageView(image: back)
+        
+    }
+    
+    
+    
 }
+
 
